5900f295bee4a49919775d7d05168c01724e124b
piri <pyoram25@gmail.com>
2024-06-23 14:14:46 +0900

    Initial Commit

---
+++ UIKitWeek2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UIKitWeek2
+//
+//  Created by ram on 6/23/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
