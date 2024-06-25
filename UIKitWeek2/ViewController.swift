0d97eb1ac96c7bdab068ff9f6170b83cdd2cfc74
piri <pyoram25@gmail.com>
2024-06-25 23:39:45 +0900

    처음엔 두번 눌러야 이미지가 바뀜

--- UIKitWeek2/ViewController.swift
+++ UIKitWeek2/ViewController.swift
@@ -8,29 +8,24 @@ import UIKit
 
 class ViewController: UIViewController {
     @IBOutlet var squareButton: [UIButton]!
-    
+    var checkBoxes: [UIButton] = []
     @IBOutlet weak var containerView: CircleCheckBox!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
         self.squareButton.forEach {
-            $0.addTarget(self, action: #selector(self.squareButton(_ :)), for: .touchUpInside)
+            $0.addTarget(self, action: #selector(squareButton(_:)), for: .touchUpInside)
         }
     }
     
-    @IBAction func squareButton(_ sender: UIButton) {
+    @objc func squareButton(_ sender: UIButton) {
         print("태그 번호: \(sender.tag)")
         
-        self.squareButton.forEach{
-            if $0.tag == sender.tag {
-                $0.setImage(UIImage(systemName: "checkmark.square.fill"), for: .normal)
-            }
-            else{
-                $0.setImage(UIImage(systemName: "square"),for: .normal)
-            }
+        if sender.currentImage == UIImage(systemName: "square") {
+            sender.setImage(UIImage(systemName: "checkmark.square.fill"), for: .normal)
+        } else {
+            sender.setImage(UIImage(systemName: "square"), for: .normal)
         }
-        
     }
-    
 }
