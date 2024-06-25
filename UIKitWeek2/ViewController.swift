70964061dce1486fbb26f0bb6e0bbc8e4bb3eafe
piri <pyoram25@gmail.com>
2024-06-25 23:05:31 +0900

    블로그보고 따라한 사각 체크박스

--- UIKitWeek2/ViewController.swift
+++ UIKitWeek2/ViewController.swift
@@ -7,10 +7,30 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet var squareButton: [UIButton]!
+    
     @IBOutlet weak var containerView: CircleCheckBox!
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        self.squareButton.forEach {
+            $0.addTarget(self, action: #selector(self.squareButton(_ :)), for: .touchUpInside)
+        }
     }
+    
+    @IBAction func squareButton(_ sender: UIButton) {
+        print("태그 번호: \(sender.tag)")
+        
+        self.squareButton.forEach{
+            if $0.tag == sender.tag {
+                $0.setImage(UIImage(systemName: "checkmark.square.fill"), for: .normal)
+            }
+            else{
+                $0.setImage(UIImage(systemName: "square"),for: .normal)
+            }
+        }
+        
+    }
+    
 }
